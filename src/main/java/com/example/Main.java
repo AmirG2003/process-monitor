87578800c233077a12fb7b2559bf9ccc49fa5d04
AmirG2003 <afgoziyev@gmail.com>
2024-11-27 11:55:50 -0500

    Deleted Main.java

--- src/main/java/com/example/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-        ProcessMonitor processMonitor = new ProcessMonitor();
-        processMonitor.displayProcesses();
-    }
-}
